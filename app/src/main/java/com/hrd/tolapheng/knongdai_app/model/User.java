2e9ba0a8bd4af234061aa83a04f8806e9a46ed84
phengtola <tolapheng99@gmail.com>
2017-01-04 12:13:21 +0700

    All

--- app/src/main/java/com/hrd/tolapheng/knongdai_app/model/User.java
+++ app/src/main/java/com/hrd/tolapheng/knongdai_app/model/User.java
@@ -1,7 +1,162 @@
 package com.hrd.tolapheng.knongdai_app.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by tolapheng on 12/27/16.
  */
 public class User {
+
+    @SerializedName("USER_ID")
+    private int userId;
+    @SerializedName("USERNAME")
+    private String username;
+    @SerializedName("EMAIL")
+    private String email;
+    @SerializedName("GENDER")
+    private String gender;
+    @SerializedName("DATE_OF_BIRTH")
+    private String dateOfBirth;
+    @SerializedName("PHONENUMBER")
+    private String phoneNumber;
+    @SerializedName("PASSWORD")
+    private String password;
+    @SerializedName("POINT")
+    private int point;
+    @SerializedName("USER_IMAGE_URL")
+    private String userImageUrl;
+    @SerializedName("REGISTERED_DATE")
+    private String registeredDate;
+    @SerializedName("USER_HASH")
+    private String userHash;
+    @SerializedName("SIGN_UP_WITH")
+    private String signUpWith;
+    @SerializedName("SOCIAL_TYPE")
+    private String scoialType;
+    @SerializedName("SOCIAL_ID")
+    private String socialId;
+    @SerializedName("STATUS")
+    private String status;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public String getUserImageUrl() {
+        return userImageUrl;
+    }
+
+    public void setUserImageUrl(String userImageUrl) {
+        this.userImageUrl = userImageUrl;
+    }
+
+    public String getUserHash() {
+        return userHash;
+    }
+
+    public void setUserHash(String userHash) {
+        this.userHash = userHash;
+    }
+
+    public String getSignUpWith() {
+        return signUpWith;
+    }
+
+    public void setSignUpWith(String signUpWith) {
+        this.signUpWith = signUpWith;
+    }
+
+    public String getScoialType() {
+        return scoialType;
+    }
+
+    public void setScoialType(String scoialType) {
+        this.scoialType = scoialType;
+    }
+
+    public String getSocialId() {
+        return socialId;
+    }
+
+    public void setSocialId(String socialId) {
+        this.socialId = socialId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
 }
--- app/src/main/java/com/hrd/tolapheng/knongdai_app/util/Session.java
+++ app/src/main/java/com/hrd/tolapheng/knongdai_app/util/Session.java
@@ -1,7 +1,97 @@
 package com.hrd.tolapheng.knongdai_app.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.hrd.tolapheng.knongdai_app.model.User;
+
 /**
  * Created by tolapheng on 12/27/16.
  */
 public class Session {
+
+    /* KEY */
+    private static final String USER_ID = "USER_ID";
+    private static final String STATUS = "STATUS";
+    private static final String USERNAME = "USERNAME";
+    private static final String EMAIL = "EMAIL";
+    private static final String USER_HASH = "USER_HASH";
+    private static final String GENDER = "GENDER";
+    private static final String REGISTERED_DATE = "REGISTERED_DATE";
+    private static final String USER_IMAGE_URL = "USER_IMAGE_URL";
+    private static final String PHONE_NUMBER = "PHONENUMBER";
+    private static final String POINT = "POINT";
+    private static final String IS_LOGIN = "IS_LOGIN";
+    private static final String DATE_OF_BIRTH = "DARE_OF_BIRTH";
+
+    /* Preference */
+    private static String sharedPrefName = "userSession";
+    private static SharedPreferences sharedPref;
+    private static SharedPreferences.Editor sharedPrefEditor;
+    private static Context mContext;
+
+    public static int userId;
+    public static String username;
+    public static String email;
+    public static String gender;
+    public static String dateOfBirth;
+    public static String phoneNumber;
+    public static String password;
+    public static int point;
+    public static String userImageUrl;
+    public static String registeredDate;
+    public static String userHash;
+    public static boolean isLogin;
+
+    public static void setContext(Context ctx) {
+        mContext = ctx;
+    }
+
+    // Save User info to SharedPreferenceFile
+    public static void saveUserSession(Context context, User user) {
+
+        mContext = context;
+
+        sharedPref = mContext.getSharedPreferences(sharedPrefName, Context.MODE_PRIVATE);
+        sharedPrefEditor = sharedPref.edit();
+        sharedPrefEditor.putInt(USER_ID, user.getUserId());
+        sharedPrefEditor.putString(STATUS, user.getGender());
+        sharedPrefEditor.putString(EMAIL, user.getEmail());
+        sharedPrefEditor.putString(USERNAME, user.getUsername());
+        sharedPrefEditor.putString(USER_HASH, user.getUserHash());
+        sharedPrefEditor.putString(GENDER, user.getGender());
+        sharedPrefEditor.putString(REGISTERED_DATE, user.getRegisteredDate());
+        sharedPrefEditor.putString(USER_IMAGE_URL, user.getUserImageUrl());
+        sharedPrefEditor.putString(PHONE_NUMBER, user.getPhoneNumber());
+        sharedPrefEditor.putString(DATE_OF_BIRTH, user.getDateOfBirth());
+        sharedPrefEditor.putInt(POINT, user.getPoint());
+        sharedPrefEditor.putBoolean(IS_LOGIN, true);
+        sharedPrefEditor.apply();
+    }
+
+    // Read User info to SharedPreferenceFile
+    public static void readUserSession(Context context) {
+        mContext = context;
+        sharedPref = mContext.getSharedPreferences(sharedPrefName, Context.MODE_PRIVATE);
+        userId = sharedPref.getInt(USER_ID, 0);
+        username = sharedPref.getString(USERNAME, null);
+        email = sharedPref.getString(GENDER, null);
+        gender = sharedPref.getString(EMAIL, null);
+        dateOfBirth = sharedPref.getString(DATE_OF_BIRTH, null);
+        phoneNumber = sharedPref.getString(PHONE_NUMBER, null);
+        point = sharedPref.getInt(POINT, 0);
+        userImageUrl = sharedPref.getString(USER_IMAGE_URL, null);
+        registeredDate = sharedPref.getString(DATE_OF_BIRTH, null);
+        userHash = sharedPref.getString(USER_HASH, null);
+        isLogin = sharedPref.getBoolean(IS_LOGIN, false);
+    }
+
+    // Clear Session or clean all data from SharedPreferenceFile
+    public static void clearSession() {
+        mContext.getSharedPreferences(sharedPrefName, Context.MODE_PRIVATE).edit().clear().apply();
+    }
+
+
+
+
 }
